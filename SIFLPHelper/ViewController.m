874435646a8db186b52df018379cade71ba7ae09
ZHRMoe <ZHRMoe@ZHRMoe-rMBP.local>
2016-02-05 21:15:55 +0800

    1.2 Bug fixed. New features added.

--- SIFLPHelper/ViewController.m
+++ SIFLPHelper/ViewController.m
@@ -10,12 +10,13 @@
 
 @interface ViewController ()
 
-@property (nonatomic) NSTimeInterval timeSinceNow;
+@property (nonatomic) NSDate *estimatedTime;
 
 @property (weak, nonatomic) IBOutlet UITextField *currentLevelTextField;
 @property (weak, nonatomic) IBOutlet UITextField *currentLPTextField;
 @property (weak, nonatomic) IBOutlet UILabel *maxLPLabel;
 @property (weak, nonatomic) IBOutlet UILabel *maxEXPLabel;
+@property (weak, nonatomic) IBOutlet UILabel *maxJPEXPLabel;
 @property (weak, nonatomic) IBOutlet UILabel *estimatedTimeLabel;
 @property (weak, nonatomic) IBOutlet UIButton *remindButton;
 
@@ -33,6 +34,14 @@
     tgr.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:tgr];
     self.remindButton.hidden = YES;
+    NSArray *plistPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *plistName = [[plistPath objectAtIndex:0] stringByAppendingPathComponent:@"LastNote.plist"];
+    NSDictionary *noteDic = [NSDictionary dictionaryWithContentsOfFile:plistName];
+    if (noteDic) {
+        NSDateFormatter *formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+        self.estimatedTimeLabel.text = [formatter stringFromDate:[noteDic objectForKey:@"EstimatedTime"]];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -53,40 +62,56 @@
     NSInteger lp = [self.currentLPTextField.text integerValue];
     NSInteger maxLP = 25 + floor(MIN(level, 300) / 2) + floor(MAX(level - 300, 0) / 3);
     NSInteger maxExp = 0;
-    if (level > 33) {
-        maxExp = round([self expFunc:level] - [self expFunc:level - 33]);
-    } else {
-        maxExp = round([self expFunc:level]);
-    }
-    self.maxLPLabel.text = [NSString stringWithFormat:@"%ld", (long)maxLP];
-    self.maxEXPLabel.text = [NSString stringWithFormat:@"%ld", (long)maxExp];
-    if (lp > maxLP) {
-        UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"意味は分からない" message:@"填写的LP大于当前等级的LP上限" delegate:self cancelButtonTitle:@"好" otherButtonTitles:nil];
+    if (level <= 0) {
+        UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"意味は分からない" message:@"等级填写错误" delegate:self cancelButtonTitle:@"好" otherButtonTitles:nil];
         [av show];
-        self.remindButton.hidden = YES;
-        self.estimatedTimeLabel.text = @"LP填写错误";
+        self.maxLPLabel.text = @"Error";
+        self.maxEXPLabel.text = @"Error";
+        self.estimatedTimeLabel.text = @"Error";
     } else {
-        self.timeSinceNow = 360 * (maxLP - lp);
-        NSDate *estimatedTime = [NSDate dateWithTimeIntervalSinceNow:self.timeSinceNow];
-        NSDateFormatter *formatter = [NSDateFormatter new];
-        formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
-        self.estimatedTimeLabel.text = [formatter stringFromDate:estimatedTime];
-        self.remindButton.hidden = NO;
+        if (level > 33) {
+            maxExp = round([self expFunc:level] - [self expFunc:level - 33]);
+        } else {
+            maxExp = round([self expFunc:level]);
+        }
+        self.maxLPLabel.text = [NSString stringWithFormat:@"%ld", (long)maxLP];
+        self.maxEXPLabel.text = [NSString stringWithFormat:@"%ld", (long)maxExp];
+        if (level < 100) {
+            self.maxJPEXPLabel.text = [NSString stringWithFormat:@"%ld(日服)", (long)maxExp / 2];
+        } else {
+            self.maxJPEXPLabel.text = @"";
+        }
+        if (lp > maxLP || lp < 0) {
+            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"意味は分からない" message:@"LP填写错误" delegate:self cancelButtonTitle:@"好" otherButtonTitles:nil];
+            [av show];
+            self.remindButton.hidden = YES;
+            self.estimatedTimeLabel.text = @"Error";
+        } else {
+            NSTimeInterval timeSinceNow = 360 * (maxLP - lp);
+            self.estimatedTime = [NSDate dateWithTimeIntervalSinceNow:timeSinceNow];
+            NSDateFormatter *formatter = [NSDateFormatter new];
+            formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+            self.estimatedTimeLabel.text = [formatter stringFromDate:self.estimatedTime];
+            self.remindButton.hidden = NO;
+        }
     }
 }
 
 - (IBAction)remindButtonTouched:(id)sender {
-    [self registerLocalNotificationWithTime:self.timeSinceNow];
+    NSArray *plistPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *plistName = [[plistPath objectAtIndex:0] stringByAppendingPathComponent:@"LastNote.plist"];
+    NSMutableDictionary *noteDic = [NSMutableDictionary dictionaryWithObject:self.estimatedTime forKey:@"EstimatedTime"];
+    [noteDic writeToFile:plistName atomically:YES];
+    [self registerLocalNotificationWithTime:self.estimatedTime];
     UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"( · 8 · )" message:@"记住了，到时候会通知你的" delegate:nil cancelButtonTitle:@"好" otherButtonTitles:nil];
     [av show];
 }
 
-- (void)registerLocalNotificationWithTime:(NSTimeInterval)timeInterval {
+- (void)registerLocalNotificationWithTime:(NSDate *)estimatedTime {
     UILocalNotification *notification = [[UILocalNotification alloc] init];
-    NSDate *fireDate = [NSDate dateWithTimeIntervalSinceNow:timeInterval];
-    notification.fireDate = fireDate;
+    notification.fireDate = estimatedTime;
     notification.timeZone = [NSTimeZone defaultTimeZone];
-    notification.repeatInterval = kCFCalendarUnitSecond;
+    notification.repeatInterval = 0;
     notification.alertBody = @"LP已经恢复完啦，快去肝活动吧！Fightだよ~";
     notification.applicationIconBadgeNumber += 1;
     notification.soundName = UILocalNotificationDefaultSoundName;
@@ -98,7 +123,6 @@
                                                                                  categories:nil];
         [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
     }
-    notification.repeatInterval = 0;
     [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
